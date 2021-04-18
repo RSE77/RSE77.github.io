9b1894133c868d793a2376603ee85fbda09eea27
RSE77 <rysboards@yahoo.com>
2021-04-18 13:50:45 -0400

    imported html, js and css for personal webpage

---
+++ script.js
@@ -0,0 +1,88 @@
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
+let darkMode = true;
+const words = ["Hello...", "I'm Ryan Ekvall...", "I am a web developer...", "Thanks for stopping by...", "See more below"];
+
+let i = 0;
+let counter;
+function typeNow() {
+   let word = words[i].split("");
+   var loopTyping = function() {
+      if (word.length > 0) {
+         document.getElementById('text').innerHTML += word.shift();
+      } else {
+         deleteNow();
+         return false;
+      };
+      counter = setTimeout(loopTyping, 150);
+   };
+   loopTyping();
+};
+function deleteNow() {
+   let word = words[i].split("");
+   var loopDeleting = function() {
+      if (word.length > 0) {
+         word.pop();
+         document.getElementById('text').innerHTML = word.join("");
+      } else {
+         if (words.length > (i + 1)) {
+            i++;
+         }
+         else {
+            i = 0;
+         };
+         typeNow();
+         return false;
+      };
+      counter = setTimeout(loopDeleting, 100);
+   };
+   loopDeleting();
+};
+typeNow();
+
+
+const imageMode = (color) => {
+    image1.src = `img/undraw_portfolio_website_lidw_${color}.svg`;
+    image2.src = `img/undraw_react_y7wq_${color}.svg`;
+    image3.src = `img/undraw_Scrum_board_re_wk7v_${color}.svg`;  
+}
+
+
+const toggleDarkLightMode = (darkMode) => {
+    nav.style.backgroundColor = darkMode ? 'rgb(0 0 0 / 50%)' : 'rgb(255 255 255 / 50%)';
+    textBox.style.backgroundColor = darkMode ? 'rgb(200 200 200 / 50%)' : 'rgb(0 0 0 / 50%)';
+    toggleIcon.children[0].textContent = darkMode ? 'Dark Mode' : 'Light Mode';
+    darkMode ? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon') : toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+    darkMode ? imageMode('dark') : imageMode('light');
+}
+
+const switchTheme = (event) => {
+    if (event.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+        toggleDarkLightMode(darkMode);
+
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+        toggleDarkLightMode(false);
+    }
+}
+
+
+toggleSwitch.addEventListener('change', switchTheme);
+
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+        toggleDarkLightMode(darkMode);
+    }
+}
